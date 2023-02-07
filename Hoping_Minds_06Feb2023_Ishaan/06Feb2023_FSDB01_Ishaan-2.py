9e3931345e4b28bde468f51d3243cb6812b0963f
IDSHM <124551200+IDSHM@users.noreply.github.com>
2023-02-07 10:03:11 +0530

    Update 06Feb2023_FSDB01_Ishaan-2.py

--- Hoping_Minds_06Feb2023_Ishaan/06Feb2023_FSDB01_Ishaan-2.py
+++ Hoping_Minds_06Feb2023_Ishaan/06Feb2023_FSDB01_Ishaan-2.py
@@ -1,1 +1,19 @@
+# insertion function
+def insertion(a):
+    for i in range(1,len(a)):
+        value=a[i]
+        j = i - 1  
+        while j >= 0 and value < a[j]:  
+          a[j + 1] = a[j]  
+          j -= 1  
+          a[j + 1] = value  
+          print(a)  
 
+# inputs
+a = [10, 5, 13, 8, 2] 
+
+# unsorted
+print("The unsorted list is:", a)  
+
+# sorted
+print("The sorted a is:", insertion(a))  
