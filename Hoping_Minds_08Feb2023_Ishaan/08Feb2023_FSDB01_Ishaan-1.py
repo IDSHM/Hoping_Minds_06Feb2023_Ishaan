2dcabd16d099de3490b7ff490cd1b88092794794
IDSHM <124551200+IDSHM@users.noreply.github.com>
2023-02-08 10:06:35 +0530

    Update 08Feb2023_FSDB01_Ishaan-1.py

--- Hoping_Minds_08Feb2023_Ishaan/08Feb2023_FSDB01_Ishaan-1.py
+++ Hoping_Minds_08Feb2023_Ishaan/08Feb2023_FSDB01_Ishaan-1.py
@@ -1,12 +1,21 @@
 # inputs
 arr = [5,4,3,2,1,6,7,8,9,10]
 
+flag = False
+
 # sorting
 arr.sort()
 print(arr)
 
 # find element position
-x = 2
+x = 11
 for i in range(len(arr)):
     if arr[i]==x:
-        print("Element at",i,"position")
+      flag = True
+      break
+
+if flag == 1:
+    print('Element was found at index',i)
+else:
+    print('{} was not found.'.format(x))
+ 
