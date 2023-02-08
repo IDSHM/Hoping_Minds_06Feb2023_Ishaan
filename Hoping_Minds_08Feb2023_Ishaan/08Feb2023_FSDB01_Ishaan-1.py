d68203f1aae3746daf675d31603c7c21fc560bdb
IDSHM <124551200+IDSHM@users.noreply.github.com>
2023-02-08 09:23:23 +0530

    Update and rename search to 08Feb2023_FSDB01_Ishaan-1.py

---
+++ Hoping_Minds_08Feb2023_Ishaan/08Feb2023_FSDB01_Ishaan-1.py
@@ -0,0 +1,12 @@
+# inputs
+arr = [5,4,3,2,1,6,7,8,9,10]
+
+# sorting
+arr.sort()
+print(arr)
+
+# find element position
+x = 2
+for i in range(len(arr)):
+    if arr[i]==x:
+        print("Element at",i,"position")
